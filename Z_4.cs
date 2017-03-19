4262aec6e788c0f28522ee02e2e2bec81ba9a5f5
Glorvo <devil.ggm@gmail.com>
2017-03-19 20:50:58 +0300

    Добавлены модули Григория

---
+++ Z_4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Белоголов Григорий Дмитриевич
+// Функция по переводу натурального числа в целое 
+// Входные данные масссив int натурального числа
+// Знак числа: 1- минус, 0 - плюс.
+namespace Z_4
+{
+    static class Z_4
+    {
+        public static int[] Run(int [] input)
+        {
+            int[] output = new int[input.Length+1];
+            output[0] = 0;// Число положительное
+            for(int i=1;i<=(input.Length +1);i++)
+            {
+                output[i] = input[i - 1];
+            }
+            return output;
+        }
+    }
+}
---
+++ Z_5.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Белоголов Григорий Дмитриевич
+// Преобразование целого неотрицательного в натуральное
+// Знак числа: 1- минус, 0 - плюс.
+namespace Z_5
+{
+    static class Z_5
+    {
+        public static int[] Run(int[] input)
+        {
+            int[] output = new int[input.Length-1];
+            for (int i = 0; i < input.Length; i++)
+            {
+                output[i] = input[i + 1];
+            }
+            return output;
+        }
+    }
+}
