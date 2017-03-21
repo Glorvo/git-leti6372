80448ea749e538636ab6c322b8e8ad0d07eec9b1
darindan <delichka36@gmail.com>
2017-03-21 19:58:10 +0300

    Добавила модуль для вычисления степени многочлена

---
+++ P_6.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P_6
+{
+    /// <summary>
+    /// Считает степень многочлена;
+    /// Холод Дарья
+    /// </summary>
+    static class P_6
+    {
+        public static int Run(int[] input)
+        {
+            return (input.Length - 1); // Вычитаем x^0, коэфф. которого также есть в массиве
+        }
+    }
+}
