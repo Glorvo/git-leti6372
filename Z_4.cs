41592684c672efc37d3fd2e390d993695d3975c9
Grisha Belogolov <grixa300@yandex.ru>
2017-03-23 01:26:54 +0300

    Преобразование Натурального в Целое

---
+++ Z_4.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// Белоголов Григорий
+/// Преобразование натурального в целое
+/// </summary>
+namespace DiskMat
+{
+    static class Z_4
+    {
+        /// <summary>
+        /// На вход подается натуральное число
+        /// </summary>
+        /// <param name="input"> Натуральное число</param>
+        /// <returns>Целое число</returns>
+        public static Digit Run(Natural input)
+        {
+            return new Digit(true,input.Value).Clear();
+        }
+    }
+}
