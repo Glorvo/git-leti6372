3696c401b09ef8f157622e1fe0abd04566cfa5e7
TimonusI <ads@timophey.space>
2017-03-22 10:04:41 +0300

    Совместимый Z_2 Руслана

---
+++ Z_2.cs
@@ -0,0 +1,24 @@
+﻿namespace DiskMat
+{
+    /// <summary>
+    /// Модуль Z_2, выводит число 2,1,0 в зависимости от знака числа
+    /// Борзенко Руслан
+    /// </summary>
+    static class Z_2
+    {
+        /// <summary>
+        /// Выводит число в зависимости от знака
+        /// </summary>
+        /// <param name="input">Целое число</param>
+        /// <returns>0 - число равно 0, 1 - чило положительно, 2 - число отрицательно</returns>
+        public static int Run(Digit input)
+        {
+            if (input.Clear().Length == 0 && input[0] == 0)
+                return 0;
+            if (input.Sign)
+                return 1;
+            else
+                return 2;
+        }
+    }
+}
