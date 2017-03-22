181ea6260bb6fb38f710f0c4265a77b33da001b5
Grisha Belogolov <grixa300@yandex.ru>
2017-03-23 01:28:32 +0300

    Преобразование целого неотрицательного в натуральное

---
+++ Z_5.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// Белоголов Григорий
+/// Преобразование целого неотрицательного в натуральное
+/// </summary>
+namespace DiskMat
+{
+    static class Z_5
+    {
+        /// <summary>
+        /// На вход подается целое неотрицательное число 
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns>Натуральное число</returns>
+        public static Natural Run(Digit input)
+        {
+            return new Natural(input.Value).Clear();
+        }
+    }
+}
