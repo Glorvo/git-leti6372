1a8bbca81f5d0267ec9de3169bf91d86ccfb0033
TimonusI <ads@timophey.space>
2017-03-22 09:50:56 +0300

    Обновлен Digits.cs, загружен совместимый с ним N_2

--- Digits.cs
+++ Digits.cs
@@ -10,6 +10,75 @@ namespace DiskMat
     /// Натуральное число. Содержат в себе знак числа и его разряды.
     /// Хаханов Тимофей
     /// </summary>
+    public class Natural
+    {
+        /// <summary>
+        /// Разряды числа
+        /// </summary>
+        public int[] Value;
+
+        /// <summary>
+        /// Доступ к длине массива разрядов по .Lenght
+        /// </summary>
+        public int Length
+        {
+            get { return Value.Length; }
+        }
+
+        /// <summary>
+        /// Инициализация положительного числа
+        /// </summary>
+        /// <param name="Arr">Массив разрядов</param>
+        public Natural(int[] Arr)
+        {
+            Value = Arr;
+        }
+
+        /// <summary>
+        /// Вспомогательная функция, очищающая "лишние" нули: 0234 станет 234
+        /// </summary>
+        /// <returns>Для удобства, возвращает себя же</returns>
+        public Natural Clear()
+        {
+            //0013 = {3100}
+            int counter = 0;
+            for (int i = Value.Length - 1; i >= 0 && Value[i] == 0; --i)
+                counter++;
+            Value = SubArray(Value, Value.Length  - counter);
+            if (Value.Length == 0)
+                Value = new int[] { 0 };
+            return this;
+        }
+
+        /// <summary>
+        /// Вспомогательная функция, "вырезает" часть массива
+        /// </summary>
+        /// <param name="data">Обрезаемый массив</param>
+        /// <param name="length">Необходимая длина</param>
+        /// <returns></returns>
+        static int[] SubArray(int[] data, int length)
+        {
+            int[] result = new int[length];
+            Array.Copy(data, 0, result, 0, length);
+            return result;
+        }
+
+        /// <summary>
+        /// Перезагрузка оператора [], позволяющая получить доступ к значению через аналогичный массиву синтаксис
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public int this[int num]
+        {
+            get { return Value[num]; }
+            set { Value[num] = value; }
+        }
+    }
+
+    /// <summary>
+    /// Целое число. Содержат в себе знак числа и его разряды.
+    /// Хаханов Тимофей
+    /// </summary>
     public class Digit
     {
         /// <summary>
@@ -20,15 +89,14 @@ namespace DiskMat
         /// <summary>
         /// Разряды числа
         /// </summary>
-        public int[] Data;
+        public Natural Value;
 
         /// <summary>
-        /// Инициализация положительного числа
+        /// Доступ к длине массива разрядов по .Lenght
         /// </summary>
-        /// <param name="Arr">Массив разрядов</param>
-        public Digit(int[] Arr)
+        public int Length
         {
-            Data = Arr;
+            get { return Value.Length; }
         }
 
         /// <summary>
@@ -39,8 +107,59 @@ namespace DiskMat
         public Digit(bool sign, int[] Arr)
         {
             Sign = sign;
-            Data = Arr;
+            Value = new Natural(Arr);
         }
+
+        /// <summary>
+        /// Инициализация числа любого знака
+        /// </summary>
+        /// <param name="sign">Знак числа (true - плюс, false - минус)</param>
+        /// <param name="data">Модуль числа</param>
+        public Digit(bool sign, Natural data)
+        {
+            Sign = sign;
+            Value = data;
+        }
+
+        /// <summary>
+        /// Инициализация положительного числа
+        /// </summary>
+        /// <param name="Arr">Массив разрядов</param>
+        public Digit(int[] Arr)
+        {
+            Value = new Natural(Arr);
+        }
+
+        /// <summary>
+        /// Инициализация положительного числа
+        /// </summary>
+        /// <param name="data">Модуль числа</param>
+        public Digit(Natural data)
+        {
+            Value = data;
+        }
+
+        /// <summary>
+        /// Вспомогательная функция, очищающая "лишние" нули: -0234 станет -234
+        /// </summary>
+        /// <returns>Для удобства, возвращает себя же</returns>
+        public Digit Clear()
+        {
+            Value.Clear();
+            return this;
+        }
+
+        /// <summary>
+        /// Перезагрузка оператора [], позволяющая получить доступ к значению через аналогичный массиву синтаксис
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public int this[int num]
+        {
+            get { return Value[num]; }
+            set { Value[num] = value; }
+        }
+
     }
 
     /// <summary>
@@ -77,6 +196,38 @@ namespace DiskMat
         {
             Numerator = num;
             Denominator = denom;
+
+            if (!(num.Sign || denom.Sign))
+                Denominator.Sign = false;
+        }
+
+        /// <summary>
+        /// Вспомогательная функция, очищающая "лишние" нули: -0234 станет -234
+        /// </summary>
+        /// <returns>Для удобства, возвращает себя же</returns>
+        public Rational Clear()
+        {
+            Numerator.Clear();
+            Denominator.Clear();
+            return this;
         }
     }
+
+   /* public class Polynomial
+    {
+        public Rational[] Values;
+
+        /// <summary>
+        /// Вспомогательная функция, очищающая "лишние" нули: -0234 станет -234
+        /// </summary>
+        /// <returns>Для удобства, возвращает себя же</returns>
+        public Polynomial Clear()
+        {
+            foreach (Rational R in Values)
+            {
+                R.Clear();
+            }
+            return this;
+        }
+    }*/
 }
--- N_2.cs
+++ N_2.cs
@@ -15,16 +15,37 @@ namespace DiskMat
         /// <summary>
         /// Проверка на ноль.
         /// </summary>
-        /// <param name="input"> Массив цифр натурального числа </param>
+        /// <param name="input">Натуральное число</param>
         /// <returns> True, если число равно нулю; False, если число не равно нулю. </returns>
-        public static bool Run(int[] input)
+        public static bool Run(Natural input)
         {
+            input.Clear();//Предворительно очищаем массив от лишних нулей
             for (int i = 0; i < input.Length; i++)
             {
                 if (input[i] != 0)
                     return false;
             }
             return true;
+        }
+
+        /// <summary>
+        /// Проверка на ноль.
+        /// </summary>
+        /// <param name="input">Целое число</param>
+        /// <returns> True, если число равно нулю; False, если число не равно нулю. </returns>
+        public static bool Run(Digit input)
+        {
+            return Run(input.Value);
+        }
+
+        /// <summary>
+        /// Проверка на ноль.
+        /// </summary>
+        /// <param name="input">Рациональное число</param>
+        /// <returns> True, если число равно нулю; False, если число не равно нулю. </returns>
+        public static bool Run(Rational input)
+        {
+            return Run(input.Numerator);
         }
     }
 }
