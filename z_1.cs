63c236945955c94e4d1af484bf3854ea5df1c9af
Tanchik28 <tankamanka_doros@inbox.ru>
2017-03-24 22:37:19 +0300

    Z_1

---
+++ z_1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// Дорош Татьяна
+/// Модуль z_1, Выводит модуль целого числа
+/// </summary>
+
+namespace DiskMat
+{
+    static class z_1
+    {
+         /// <summary>
+        /// выводится модуль числа
+       /// </summary>
+      /// <param name="input">целое число</param>
+     /// <returns>модуль числа</returns> 
+        public static Natural Run (Digit input)
+        {
+            return input.Value;
+        }
+    }
+}
