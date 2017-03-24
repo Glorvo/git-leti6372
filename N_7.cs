da8c6f8858527e37d1b0eaa79dba4ae8824cc03b
Glorvo <devil.ggm@gmail.com>
2017-03-24 15:40:36 +0300

    Добавлен модуль Дмитрия Шашкова

---
+++ N_7.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DiskMat
+
+    {
+        /// <summary>
+        /// Модуль N_7, умножает число на 10^k
+        /// Шашков Дмитрий
+        /// </summary>
+        /// 
+        static class N_7
+           {
+            /// <summary>
+            /// Умноженает исходное число на 10^k
+            /// </summary>
+            /// <param name="k">Степень в которую возводится 10</param>
+            /// <param name="newArr">новый массив в который перезаписывается исходный</param>
+
+            public static Natural Run(Natural input, int k)
+
+                {
+
+                    int[] newArr = new int[input.Length + k]; //создание массива большоего на k элементов
+
+                    input.Value.CopyTo(newArr, k); //Переносим число в расширенный массив с нуными нулями
+
+                    return new Natural(newArr); //Возвращаям измененный массив
+                }
+
+            }
+    }
+
