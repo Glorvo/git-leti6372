f0c14f13705a2b35f9370c93dde0bce404f4b9ca
Glorvo <devil.ggm@gmail.com>
2017-03-28 22:40:20 +0300

    Добавлена проверка числа на целое

---
+++ Q_2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiskMat
+{
+    /// <summary>
+    /// Проверка число на целое
+    /// Кузнецов Владислав
+    /// </summary>
+    static class Q_2
+    {
+        /// <summary>
+        /// Проверка на целое, если рациональное число является целым, то «да», иначе «нет»
+        /// </summary>
+        /// <param name="Rational input">вводится рациональное число</param>
+        /// <returns>"true" или "false"</returns>
+        public static bool Run(Rational input)
+        {
+            return input.Denominator.Clear().Length == 1 && input.Denominator[0] == 1;
+
+        }
+    }
+}
