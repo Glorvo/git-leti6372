04a5c3ae7ab54d871424df5b8473f3417cdf159d
Grisha Belogolov <grixa300@yandex.ru>
2017-03-31 15:25:20 +0300

    Деление дробей

---
+++ Q_8.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// Белоголов Григорий Дмитриевич
+/// Деление дробей
+/// </summary>
+namespace DiskMat
+{
+        public static class Q_8
+        {
+            /// <summary>
+            /// Деление дробей
+            /// </summary>
+            /// <param name="A">Первая дробь </param>
+            /// <param name="B">Вторая дробь </param>
+            /// <returns>Поделенные дроби </returns>
+            public static Rational Run(Rational A, Rational B)
+            {
+            Rational output = new Rational(new Digit(new int[] {  }),new Digit(new int[] { }));// создаем локальную переменную
+            output.Numerator = Z_8.Run(A.Numerator,B.Denominator);// определяем числитель новой дроби
+            output.Denominator = Z_8.Run(A.Denominator, B.Numerator);// определяем знаментатель
+            return output;
+            }
+
+        }
+}
