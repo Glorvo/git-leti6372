0ff748acc0e72f3e969e968180d64cbc5e9fa05d
TimonusI <ads@timophey.space>
2017-03-31 16:07:37 +0300

    Z_3

---
+++ Z_3.cs
@@ -0,0 +1,20 @@
+﻿namespace DiskMat
+{
+    /// <summary>
+    /// Умножение целого на (-1)
+    /// Першин Кирилл
+    /// </summary>
+    public static class Z_3
+    {
+        /// <summary>
+        /// Умножение целого на (-1)
+        /// </summary>
+        /// <param name="A"> множитель</param>
+        /// <returns>Умноженное число</returns>
+        public static Digit Run(Digit A)
+        {
+            return new Digit(!A.Sign, A.Value);
+        }
+
+    }
+}
