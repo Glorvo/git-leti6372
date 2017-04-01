b321316abc96ba230b345fd70fe0ebc14fdf5035
TOXIC <baranda@mail.ru>
2017-04-01 16:35:10 +0300

    Сорян, не ту версию залил. Окончательный вариант Вычисления первой цифры от деления большего натуральнего на меньшее и умножение ее на 10^k, где k - ее порядок

--- N_10.cs
+++ N_10.cs
@@ -10,11 +10,10 @@ namespace DiskMat
     /// Модуль N_10 Вычисление первой цифры деления и умножение ее на 10^k, где k - ее разряд
     /// Бараняк Данила
     /// </summary>
-
     class N_10
     {
         /// <summary>
-        /// Вычисление первой цифры деления и умножение ее на 10^k, где k - ее разряд
+        /// Вычисление первой цифры деления и умножение ее на 10^k, где k - ее разряд (второе число не меньше первого)
         /// </summary>           
         /// <param name="n1">Первое натуральное число</param>
         /// <param name="n2">Второе натуральное число</param>
@@ -23,36 +22,31 @@ namespace DiskMat
         {
             
 
-            n1.Clear(); n2.Clear();
-            
-            if (N_1.Run(n1, n2) == 1)
-            {
-                Natural temp = n1;
-                n1 = n2;
-                n2 = temp;
-            }
+            n1.Clear(); n2.Clear();            
 
-            Natural value = new Natural(new int[n2.Length]);
+            if (N_1.Run(n1, n2) == 2) throw new Exception();                            // Если первое число меньше второго, то ошибка
+
+            Natural temp = new Natural(new int[1]);
             int k = 0;
-            
-            while (N_1.Run(value, n2) == 1)
+            do
             {
-
-                value.Value[k] = n1.Value[k];
+                if (temp.Length == k) temp = new Natural(new int[temp.Length + 1]);     // Если длинна передаваемого числа болььше длинны массива, увеличить его на 1
+                
+                for (int p = 0; p <= k; p++)                                            // Передача числа по разрядно из входящего во временный
+                {
+                    temp.Value[temp.Length - p - 1] = n1.Value[n1.Length - p - 1];
+                }
+                
                 k++;
-            }
-            
-            int i;
-            for (i = 0; i < 10; i++)
-            {
-                if (N_1.Run(N_6.Run(value, i + 1), n2) != 1)
-                    break;
-            }
-            if (i == 0) i = 1;
+            } while (N_1.Run(temp, n2) == 2) ;
 
-            int[] result = new int[k];
-            result[0] = i;
-            return new Natural(result);
+            int i = 1;
+            while (N_1.Run(N_6.Run(n2, ++i), temp) != 1) ;                              // Вычисление первой цифры от деления чисел
+
+            Natural result = new Natural(new int[n1.Length - k + 1]);
+            result[result.Length - 1] = i - 1;
+
+            return result;
         }
     }       
 }
