61c26e7579bf087f791adb49367d63fbb7dab1b4
Vampika <vampika.vamp@gmail.com>
2017-04-03 23:39:43 +0300

    Add files via upload

---
+++ Q_3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Модуль преобразования целого в дробное
+/// Сесин Иван
+/// </summary>
+namespace DiskMat
+{
+    class Q_3
+    {
+        /// <summary>
+        /// Перевод из целого в дробное
+        /// В знаменателе "1"
+        /// </summary>
+        /// <param name="input">Целое число</param>
+        /// <returns>Дробное число</returns>
+        public static Rational Run(Digit input)
+        {
+            return new Rational(input);
+        }
+    }
+}
