4081d72d0396d2b61e422d55bf865896bd020a97
Glorvo <devil.ggm@gmail.com>
2017-04-04 21:11:05 +0300

    исправлен небольшой баг

--- Q_1.cs
+++ Q_1.cs
@@ -19,12 +19,12 @@ namespace DiskMat
         /// <returns>Будет возвращенa сокращенная дробь </returns>
         public static Rational Run(Rational A)
         {
-            if (A.Clear().Numerator[0] == 0)
+            if (A.Clear().Numerator[0] == 0 && A.Numerator.Length == 1)
                 return new Rational("0");
             Natural HOD = N_13.Run(Z_5.Run(new Digit(A.Denominator)), Z_5.Run(A.Numerator));// Находим НОД числителя и знаменателя
-            A.Numerator = Z_9.Run(A.Numerator,HOD);//Сокращаем числитель на НОД числителя и знаменятеля
+            A.Numerator = Z_9.Run(A.Numerator, HOD);//Сокращаем числитель на НОД числителя и знаменятеля
             A.Denominator = N_11.Run(A.Denominator, HOD);// Сокращаем знаменатель на нод
-            
+
             return A;
         }
     }
