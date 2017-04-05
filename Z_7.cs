0c18f06d2e815a61b6f84d29b34a2dc4561e4bc2
timonusi <ads@timophey.space>
2017-04-05 13:09:51 +0300

    Ремастеред Z_7: теперь в одну строку и точно работает

--- Z_7.cs
+++ Z_7.cs
@@ -20,61 +20,7 @@ namespace DiskMat
         /// <returns>Разность</returns>
         public static Digit Run(Digit A, Digit B)
         {
-            Digit result = new Digit("0");
-            result.Sign = true;
-
-            int len = Math.Min(A.Length, B.Length);
-            switch (N_1.Run(A.Value, B.Value))
-            {
-                case 1:
-                    if (A.Sign == B.Sign)
-                        result = Dif(A, B, len);
-                    else
-                        result.Value = N_4.Run(A.Value, B.Value);
-                    result.Sign = A.Sign;
-                    break;
-
-                case 2:
-                    if (A.Sign == B.Sign)
-                        result = Dif(B, A, len);
-                    else
-                        result.Value = N_4.Run(A.Value, B.Value);
-                    result.Sign = !B.Sign;
-                    break;
-
-                case 0:
-                    if (A.Sign != B.Sign)
-                        result = new Digit(N_6.Run(A.Value, 2));
-                    result.Sign = A.Sign;
-                    break;
-            }
-            return result.Clear();
-        }
-
-
-        /// <summary>
-        /// Функция вычитания
-        /// </summary>
-        static Digit Dif(Digit D1, Digit D2, int len)
-        {
-            int[] a = new int[D1.Value.Length];
-            for (int i = 0; i < D1.Value.Length; i++)
-            {
-                a[i] = D1.Value[i];
-            }
-            Digit Result = new Digit(true, a);
-            for (int i = len - 1; i >= 0; i--)
-            {
-
-                Result[i] -= D2[i];                         // Поразрядное вычитание
-
-                if (Result[i] < 0)                          // Если меньше нуля, занять десятку у более высокого разряда
-                {
-                    Result[i] += 10;
-                    Result[i + 1]--;
-                }
-            }
-            return Result;
+            return Z_6.Run(A, new Digit(!B.Sign, B.Value));
         }
     }
 }
