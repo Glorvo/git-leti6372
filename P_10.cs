06375349042c29fff6a84ce7568cfdb40ad25de2
TOXIC <baranda@mail.ru>
2017-04-06 08:38:52 +0300

    P_10

--- P_10.cs
+++ P_10.cs
@@ -11,11 +11,15 @@ namespace DiskMat
         /// <returns>Остаток</returns>
         public static Polynomial Run(Polynomial A, Polynomial B)
         {
+            int i = A.Values.Length;
+            Polynomial C = new Polynomial(new Rational[i]);
+            C = A;
             Rational E;
-            while (P_6.Run(A) >= P_6.Run(B))   // Выполняется пока можно делить(т.е. пока степень Делимого >= степ. Делителя)
+            while (A.Values.Length >= B.Values.Length)   // Выполняется пока можно делить(т.е. пока степень Делимого >= степ. Делителя)
             {
                 E = Q_8.Run(P_5.Run(A), P_5.Run(B));  //Находим коэфициент частного(Е), который будем умнажать на делитель
-                A = P_2.Run(A, (P_3.Run(B, E)));   // Вычитаем из Делимого произведение Е на Делитель. И заменяем предыдущее Делимое полученным.
+                C = P_4.Run((P_3.Run(B, E)), A.Values.Length - B.Values.Length);
+                A = P_2.Run(A, C);   // Вычитаем из Делимого произведение Е на Делитель. И заменяем предыдущее Делимое полученным.
             }
             return A; // После цикла получили многочлен который не делится на Делитель(т.е. остаток)
         }
