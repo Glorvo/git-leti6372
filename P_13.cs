c898e6aef70d2f8d8900f94bf24fe7184ba5b8cb
Vampika <vampika.vamp@gmail.com>
2017-04-06 09:34:10 +0300

    Преобразование многочлена

---
+++ P_13.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiskMat
+{
+    /// <summary>
+    /// Преобразование многочлена — кратные корни в простые
+    /// Пульнева Ксения
+    /// </summary>
+    public static class P_13
+    {
+        /// <summary>
+        /// Преобразование многочлена — кратные корни в простые
+        /// </summary>
+        
+
+       /* public static Polynomial Run(Polynomial A)
+        {
+            Polynomial Der = P_12.Run(A);
+            Polynomial Gcd =P_11.Run(A, Der);
+            return P_9.Run(A, Gcd);
+        }*/
+
+    }
+}
